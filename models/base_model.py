fcb54140c26d221de7594e8064ea4052b7291ad7
RamziHkiri <6257@holbertonstudents.com>
2023-07-12 14:27:59 +0200

    base model file

--- models/base_model.py
+++ models/base_model.py
@@ -3,19 +3,29 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
+    """defile the base model methods and fields"""
+
     def __init__(self):
+        """the base model constructor"""
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self) -> str:
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        """string representation of base model"""
+        return ("[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
+        """update the time of the model"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """dictionnary representation"""
+
         diction = self.__dict__.copy()
         diction["__class__"] = self.__class__.__name__
         diction["created_at"] = self.created_at.isoformat()
