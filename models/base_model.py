bc59bdfb28d4459338c32d154674d30ff219b103
RamziHkiri <6257@holbertonstudents.com>
2023-07-12 15:15:42 +0200

    update the init function that we can use kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -7,12 +7,20 @@ from datetime import datetime
 class BaseModel:
     """defile the base model methods and fields"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """the base model constructor"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs and type(kwargs) == dict and len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "id":
+                        setattr(self, key, value)
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.fromisoformat(value))
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self) -> str:
         """string representation of base model"""
