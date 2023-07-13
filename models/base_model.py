649c3ec200a187191b18a1b33a3f9cffed17015e
RamziHkiri <6257@holbertonstudents.com>
2023-07-13 15:34:01 +0200

    fixing into init model storage .new()

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            storage.new()
+            storage.new(self)
 
     def __str__(self) -> str:
         """string representation of base model"""
@@ -32,7 +32,7 @@ class BaseModel:
     def save(self):
         """update the time of the model"""
         self.updated_at = datetime.now()
-        storage.save(self)
+        storage.save()
 
     def to_dict(self):
         """dictionnary representation"""
